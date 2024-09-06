b9abd290b7e00aa77ac1c3390be312c9f39969b5
benjaaminh <benjamin.heikkala@abo.fi>
2024-09-06 15:20:51 +0300

    10.10

--- rate-repository-app/src/components/Main.jsx
+++ rate-repository-app/src/components/Main.jsx
@@ -3,12 +3,14 @@ import { Route, Routes, Navigate } from 'react-router-native';
 import RepositoryList from './RepositoryList';
 import AppBar from './AppBar';
 import SignIn from './SignIn';
+import theme from '../theme';
 
 const styles = StyleSheet.create({
   container: {
     flexGrow: 1,
     flexShrink: 1,
-    backgroundColor: "lightgrey"
+    backgroundColor: "lightgrey",
+    fontFamily: theme.fonts.main
   },
 });
 
--- rate-repository-app/src/components/SignIn.jsx
+++ rate-repository-app/src/components/SignIn.jsx
@@ -1,23 +1,16 @@
-import { StyleSheet, View, TextInput, Pressable } from "react-native";
+import { StyleSheet, View, Pressable } from "react-native";
+import TextInput from "./TextInput";
 import { useFormik } from 'formik';
 import theme from "../theme";
 import Text from "./Text";
+import * as yup from "yup"
+
 
 const styles = StyleSheet.create({
     container: {
         flexGrow: 1,
         flexShrink: 1,
         backgroundColor: "lightgrey"
-    },
-    textinput: {
-        margin: 10,
-        paddingHorizontal: 10,
-        paddingVertical: 4,
-        borderWidth: 1,
-        borderRadius: 9,
-        borderColor: "lightgrey",
-        color: "grey"
-
     },
     button: {
         backgroundColor: theme.colors.primary,
@@ -26,6 +19,10 @@ const styles = StyleSheet.create({
         paddingVertical: 6,
         margin: 10,
         borderRadius: 6
+    },
+    errorText: {
+        color: "#d73a4a",
+        marginLeft: 10
     }
 });
 
@@ -34,26 +31,44 @@ const initialValues = {
     password: '',
 };
 
+const validationSchema = yup.object().shape({
+    username: yup
+        .string()
+        .required('Username is required'),
+    password: yup
+        .string()
+        .required('Password is required'),
+});
+
 const SignInForm = ({ onSubmit }) => {
     const formik = useFormik({
         initialValues,
         onSubmit,
+        validationSchema,
     });
 
     return (
         <View style={styles.container}>
-            <View style={{backgroundColor: "white"}}>
-                <TextInput style={styles.textinput}
+            <View style={{ backgroundColor: "white" }}>
+                <TextInput
+                    error={formik.errors.username}
                     placeholder="Username"
                     value={formik.values.username}
                     onChangeText={formik.handleChange('username')}
                 />
-                <TextInput style={styles.textinput}
+                {formik.touched.username && formik.errors.username && (
+                    <Text style={styles.errorText}>{formik.errors.username}</Text>
+                )}
+                <TextInput
+                    error={formik.errors.password}
                     secureTextEntry={true}
                     placeholder="Password"
                     value={formik.values.password}
                     onChangeText={formik.handleChange('password')}
                 />
+                {formik.touched.password && formik.errors.password && (
+                    <Text style={styles.errorText}>{formik.errors.password}</Text>
+                )}
                 <Pressable style={styles.button} onPress={formik.handleSubmit}>
                     <Text style={{ color: "white" }}>Sign in</Text>
                 </Pressable>
